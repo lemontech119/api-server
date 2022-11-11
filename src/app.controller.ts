c7eb47956cb0b369bb8d661dd4e865f752497e11
AhnDongwon <lemontech119@gmail.com>
2022-11-11 16:37:00 +0900

    Setting(Project): swagger 세팅

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
+@ApiTags('테스트 API')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: '서버 확인용 API', description: '확인용입니다.' })
+  @ApiResponse({ description: 'Hello World!' })
   getHello(): string {
     return this.appService.getHello();
   }
