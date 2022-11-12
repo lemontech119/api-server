bb2bb3b3d269a99be84d26d809c7c98cdbe81c44
AhnDongwon <lemontech119@gmail.com>
2022-11-12 15:06:50 +0900

    Update(typeorm): datasource 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -24,4 +25,6 @@ import { ConfigModule } from '@nestjs/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
