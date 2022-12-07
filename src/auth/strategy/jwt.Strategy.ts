74e383ff3b47f61255e706681a2dd798152f73f4
KwonHyeokJoo <gagaKor@gmail.com>
2022-12-07 16:53:23 +0900

    세팅 변경

--- src/auth/strategy/jwt.Strategy.ts
+++ src/auth/strategy/jwt.Strategy.ts
@@ -14,6 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: 'test',
+      passReqToCallback: true,
     });
   }
 
--- src/decorator/get-user.decorator.ts
+++ src/decorator/get-user.decorator.ts
@@ -1,6 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GetUserDto } from './../auth/dto/getUser.dto';
 
+/**
+ * @UseGaurds(AuthGard) security 사용시에만 사용 가능
+ */
 export const GetUser = createParamDecorator(
   (data: string, ctx: ExecutionContext): GetUserDto => {
     const req = ctx.switchToHttp().getRequest();
