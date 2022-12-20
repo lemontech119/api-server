087befffc8aad3ae9ecda5b5869860c5f5f82619
KwonHyeokJoo <gagaKor@gmail.com>
2022-12-20 15:11:20 +0900

    User Decorator Pipe

--- src/decorator/get-user.decorator.ts
+++ src/decorator/get-user.decorator.ts
@@ -1,12 +1,18 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GetUserDto } from './../auth/dto/getUser.dto';
+import { ParseUserPipe } from 'src/auth/pipe/parse-user.pipe';
 
 /**
  * @UseGaurds(AuthGard) security 사용시에만 사용 가능
  */
-export const GetUser = createParamDecorator(
+export const GetUserDecorator = createParamDecorator(
   (data: string, ctx: ExecutionContext): GetUserDto => {
     const req = ctx.switchToHttp().getRequest();
     return req.user;
   },
 );
+/**
+ * Module에 AuthModule import 한 뒤 사용
+ */
+export const GetUser = (additionalOptions?: any) =>
+  GetUserDecorator(additionalOptions, ParseUserPipe);
