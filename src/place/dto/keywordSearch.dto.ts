fdbd978d3b91d0f1388d0e18d13dfe89517abb1a
KwonHyeokJoo <gagaKor@gmail.com>
2023-01-13 15:04:31 +0900

    swagger

--- src/place/dto/keywordSearch.dto.ts
+++ src/place/dto/keywordSearch.dto.ts
@@ -5,31 +5,61 @@ import {
   ReviewPraisedEnum,
 } from 'src/review_mood/review_mood.enum';
 import { KeywordEtcSearchDto } from './keywordEtcSearch.dto';
+import { ApiProperty } from '@nestjs/swagger';
 type Participants = {
   min: number;
   max: number;
 };
 
 export class KeywordSearchDto {
+  @ApiProperty({
+    example: '{main: 4, max: 8}',
+    description: '참석인원 범위',
+    type: JSON,
+  })
   @IsNumber()
   @IsOptional()
   participants: Participants;
 
+  @ApiProperty({
+    example: '3만 이상 5만 이하',
+    description: '가격 범위',
+    type: String,
+  })
   @IsString()
   @IsOptional()
   price: string;
 
+  @ApiProperty({
+    example: 'Light',
+    description: '분위기',
+    type: ReviewMoodEnum,
+  })
   @IsString()
   @IsOptional()
   mood: ReviewMoodEnum;
 
+  @ApiProperty({
+    example: 'Bright',
+    description: '조명',
+    type: ReviewLightingEnum,
+  })
   @IsString()
   @IsOptional()
   lighting: ReviewLightingEnum;
 
+  @ApiProperty({
+    example: 'Cheep',
+    description: '칭찬',
+    type: ReviewPraisedEnum,
+  })
   @IsString()
   @IsOptional()
   praised: ReviewPraisedEnum;
 
+  @ApiProperty({
+    description: '기타 조건',
+    type: KeywordEtcSearchDto,
+  })
   etc: KeywordEtcSearchDto;
 }
